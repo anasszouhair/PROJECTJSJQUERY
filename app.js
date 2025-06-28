db241ac87a7a33090e5d00108228d81ab24d7671
anasszouhair <anaszouhair@gmail.com>
2025-06-28 20:21:18 +0200

    ver1.0

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+ $('#toggle-btn').click(function(){
+    $('#my-paragraphe').toggle(1000).css({
+        color:'#000',
+        fontSize:'12px'
+    })
+    })
+
+
+
+})
+$(document).ready(function(){
+    $('#toggle-btn').click(function(){
+        $('#'+).toggle().css({
+            color:#1111,
+            fontSize:'10px'
+
+        })
+    })
+})
